b8abe83d86e748dee7346e768affbfe955c40967
Yash Uttam <ur731084@gmail.com>
2025-12-10 00:44:21 +0530

    Add:Day 3 last -

--- DSA-Leetcode/283. Move Zeroes.py
+++ DSA-Leetcode/283. Move Zeroes.py
@@ -5,3 +5,7 @@ class Solution:
             if nums[i] != 0:
                 nums[pos] = nums[i]
                 pos += 1
+
+        while pos < len(nums):
+            nums[pos] = 0
+            pos += 1
