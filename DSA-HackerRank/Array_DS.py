22133e330a112485bc0adc86aa278acb1a03e11e
Yash Uttam <ur731084@gmail.com>
2025-12-10 00:10:38 +0530

    Add:Day 3 -

--- DSA-HackerRank/Array_DS.py
+++ DSA-HackerRank/Array_DS.py
@@ -6,15 +6,10 @@ import random
 import re
 import sys
 
-#
-# Complete the 'reverseArray' function below.
-#
-# The function is expected to return an INTEGER_ARRAY.
-# The function accepts INTEGER_ARRAY a as parameter.
-#
+
 
 def reverseArray(a):
-    # Write your code here
+
     return a[::-1]
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
